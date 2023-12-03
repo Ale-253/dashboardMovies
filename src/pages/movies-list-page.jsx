3fcd21ca756494276c0fc27f4b58c2557937d676
Ale-253 <ale.santamarina@hotmail.com>
2023-12-03 12:41:03 -0300

    se agrega loading

--- src/pages/movies-list-page.jsx
+++ src/pages/movies-list-page.jsx
@@ -1,16 +1,19 @@
 import { Card, Table } from "react-bootstrap"
 import { TableItemMovies } from "../components/TableItemMovies";
 import { useEffect, useState } from "react";
+import { Loading } from "../components/loading";
 
 export const MoviesListPage = () => {
 
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
 
         const apiCall = async () => {
             const response = await fetch('http://localhost:3031/api/movies');
             const result = await response.json();
+            setLoading(false)
             setMovies(result.data)
         }
 
@@ -24,26 +27,32 @@ export const MoviesListPage = () => {
             <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">Lista de películas</h1>
             </div>
-            <Card>
-                <Card.Body>
-                    <Table stripped>
-                        <thead>
-                            <tr>
-                                <th>Título</th>
-                                <th>Duración</th>
-                                <th>Rating</th>
-                                <th>Géneros</th>
-                                <th>Premio</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {movies.map(({ title, length, genre, awards, rating }, index) => (
-                                <TableItemMovies key={index + title} title={title} length={length} genre={genre} awards={awards} rating={rating} />
-                            ))}
-                        </tbody>
-                    </Table>
-                </Card.Body>
-            </Card>
+            {
+                loading ?
+                    <Loading />
+                    :
+                    <Card>
+                        <Card.Body>
+                            <Table stripped>
+                                <thead>
+                                    <tr>
+                                        <th>Título</th>
+                                        <th>Duración</th>
+                                        <th>Rating</th>
+                                        <th>Géneros</th>
+                                        <th>Premios</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {movies.map(({ title, length, genre, awards, rating }, index) => (
+                                        <TableItemMovies key={index + title} title={title} length={length} genre={genre} awards={awards} rating={rating} />
+                                    ))}
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+            }
+
         </>
     );
 }
